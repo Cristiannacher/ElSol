f264938303115a6b7b336330ef87ffb5ee788b65
Cristiannacher <alunac6050@ieselcaminas.org>
2022-10-26 13:20:07 +0200

    casa

--- app/src/main/java/com/example/elsol/MainActivity.kt
+++ app/src/main/java/com/example/elsol/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.elsol
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -10,9 +11,11 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
 
         val items = ArrayList<Sol>()
@@ -42,6 +45,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId.equals(R.id.compara)) {
+            val i = Intent(this, ComparaActivity::class.java)
+            startActivity(i)
+        }
         return true
     }
+
 }
